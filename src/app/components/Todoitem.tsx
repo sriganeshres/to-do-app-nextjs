904cb581b0a8c6170c4d330c15533feb43e2ad9b
sriganeshres <sriganeshthota12345@gmail.com>
2023-08-19 23:05:05 +0530

    Final Commit ig

--- src/app/components/Todoitem.tsx
+++ src/app/components/Todoitem.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 interface Props {
     id: string
     title: string
--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,7 +3,6 @@ import { prisma } from "@/db"
 import { redirect } from "next/navigation"
 
 async function createTodo(data: FormData) {
-    "use server"
     const title = data.get('title')?.valueOf()
     if (typeof title !== "string" || title.length === 0) {
         throw new Error("Invalid Code")
