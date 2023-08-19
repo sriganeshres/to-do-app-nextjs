00d37d2c4c71af783aecd282ffc0199267d6dd61
sriganeshres <sriganeshthota12345@gmail.com>
2023-08-19 23:17:40 +0530

    Final Commit ig

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/db"
 import { redirect } from "next/navigation"
 
 async function createTodo(data: FormData) {
+    "use server"
     const title = data.get('title')?.valueOf()
     if (typeof title !== "string" || title.length === 0) {
         throw new Error("Invalid Code")
