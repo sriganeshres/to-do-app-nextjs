11989a281f8e66e512cbfe49180487fadc1e511c
sriganeshres <sriganeshthota12345@gmail.com>
2023-08-19 23:57:43 +0530

    Finalized changes

--- src/app/components/Todoitem.tsx
+++ src/app/components/Todoitem.tsx
@@ -1,3 +1,4 @@
+"use client"
 interface Props {
     id: string
     title: string
@@ -6,9 +7,12 @@ interface Props {
 }
 
 const Todoitem = ({ id, title, complete, toggleTodo }: Props) => {
+    const handleCheck = (e: React.FormEvent<HTMLInputElement>) => {
+        toggleTodo(id, e.currentTarget.checked)
+    }
     return (
         <li className="flex gap-1 items-center">
-            <input id={id} type="checkbox" className="cursor-pointer peer" defaultChecked={complete} onChange={e => { toggleTodo(id, e.target.checked) }} />
+            <input id={id} type="checkbox" className="cursor-pointer peer" defaultChecked={complete} onChange={handleCheck} />
             <label htmlFor={id} className=" cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
         </li>
     )
